6e759bc926e948b3c5c7826c3cb39ea4ef8242f9
flamewalker103-commits <ouchalesho@hotmail.com>
2025-09-26 06:40:45 -0500

    feat: initial commit for src/calculadora.js

---
+++ src/calculadora.js
@@ -0,0 +1,22 @@
+class Calculadora {
+  sumar(a, b) {
+    return a + b;
+  }
+
+  restar(a, b) {
+    return a - b;
+  }
+
+  multiplicar(a, b) {
+    return a * b;
+  }
+
+  dividir(a, b) {
+    if (b === 0) {
+      throw new Error('División por cero no permitida.');
+    }
+    return a / b;
+  }
+}
+
+module.exports = { Calculadora };
